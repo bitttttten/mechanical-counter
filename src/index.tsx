220832595118f64e85204bdb7faaaa6ffb0e7262
bitttttten <bitttttten@googlemail.com>
2021-10-23 11:50:17 +0200

    use import react

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { Vertical } from "./vertical";
--- src/vertical.tsx
+++ src/vertical.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { motion } from "framer-motion";
 
 interface VerticalProps {
